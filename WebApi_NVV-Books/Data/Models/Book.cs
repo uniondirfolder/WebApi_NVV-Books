c1fcc816df3572a8f510406b2175e02793b2529a
NVV <uniondirfolder@gmail.com>
2021-03-19 16:48:45 -0700

    One-to-Many Relationships in Entity Framework Core

--- WebApi_NVV-Books/Data/Models/Book.cs
+++ WebApi_NVV-Books/Data/Models/Book.cs
@@ -14,5 +14,9 @@ namespace WebApi_NVV_Books.Data.Models
         public string Author { get; set; }
         public string CoverUrl { get; set; }
         public DateTime DateAdded { get; set; }
+
+        //Navigation Properties
+        public int PublisherId { get; set; }
+        public Publisher Publisher { get; set; }
     }
 }
---
+++ WebApi_NVV-Books/Data/Models/Publisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_NVV_Books.Data.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Navigation Properties
+        public List<Book> Books { get; set; }
+    }
+}
