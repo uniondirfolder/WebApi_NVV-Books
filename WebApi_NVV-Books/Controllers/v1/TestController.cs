9bca82af4a0b7be6ea1a0da2fe96d3ccd77d28ab
NVV <uniondirfolder@gmail.com>
2021-03-20 14:38:09 -0700

    Asp.NET Core Web API Versioning

---
+++ WebApi_NVV-Books/Controllers/v1/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_NVV_Books.Controllers.v1
+{
+    [ApiVersion("2.0")]
+    [Route("api/[controller]")]
+    //[Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("get-test-data")]
+        public IActionResult Get() 
+        {
+            return Ok("This is TestController v2");
+        }
+    }
+}
+
+//aspnetcore.mvc.versioning
--- WebApi_NVV-Books/Startup.cs
+++ WebApi_NVV-Books/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -43,6 +44,15 @@ namespace WebApi_NVV_Books
             services.AddTransient<BooksService>();
             services.AddTransient<AuthorsService>();
             services.AddTransient<PublishersService>();
+
+            services.AddApiVersioning(config=> {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+
+                //config.ApiVersionReader = new HeaderApiVersionReader("custom-version-header");
+                //config.ApiVersionReader = new MediaTypeApiVersionReader();
+                //->querystring default versining type
+            });
 
             services.AddSwaggerGen(c =>
             {
